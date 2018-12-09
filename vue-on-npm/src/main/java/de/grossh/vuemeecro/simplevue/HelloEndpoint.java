7d40a901f8f22bc3b147cd9b18dabd313bc88762
grossh <grossh@gmx.de>
2018-12-09 18:44:27 +0100

    Add npm build and config files. Add java content

---
+++ vue-on-npm/src/main/java/de/grossh/vuemeecro/simplevue/HelloEndpoint.java
@@ -0,0 +1,18 @@
+package de.grossh.vuemeecro.simplevue;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("hello")
+@ApplicationScoped
+public class HelloEndpoint {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String sayHi() {
+        return "Hello World";
+    }
+}
